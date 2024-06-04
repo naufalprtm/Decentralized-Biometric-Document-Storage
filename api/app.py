4f7500b4f9c853b2373a34dbfbb1350598228e3d
naufalprtm <pratamanaufal512@gmail.com>
2024-06-05 04:35:17 +0700

    Update project with latest changes

--- api/app.py
+++ api/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from tinybio_integration import TinyBioIntegration
+from http.server import BaseHTTPRequestHandler
+from os.path import dirname, abspath, join
+dir = dirname(abspath(__file__))
 
 app = Flask(__name__)
 CORS(app)
